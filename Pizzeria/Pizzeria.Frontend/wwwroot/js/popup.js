032a6ef380f8d1cba54781688447683c7d55951a
jvas04 <cortelamp2004@gmail.com>
2024-11-14 17:04:37 -0500

    Creacion Visual entera de promociones

---
+++ Pizzeria/Pizzeria.Frontend/wwwroot/js/popup.js
@@ -0,0 +1,36 @@
+﻿// popup.js
+
+function mostrarPopup() {
+    Swal.fire({
+        title: '<div class="popup-titulo">Selecciona pizza para La Reunión Perfecta</div>',
+        html: `
+            <div class="popup-container">
+                <div class="popup-card">
+                    <div class="promocion-imagen"><img src="images/cuatro quesos.jpeg" class="promocion-imagen" /></div>
+                    <h3>Pizza Cuatro Quesos</h3>
+                    <button>Agregar al carrito</button>
+                </div>
+                <div class="popup-card">
+                    <div class="promocion-imagen"><img src="images/vegetar.jpeg" class="promocion-imagen" /></div>
+                    <h3>Pizza Vegetariana</h3>
+                    <button>Agregar al carrito</button>
+                </div>
+                <div class="popup-card">
+                    <div class="promocion-imagen"><img src="images/ranchera.jpeg" class="promocion-imagen" /></div>
+                    <h3>Pizza Ranchera</h3>
+                    <button>Agregar al carrito</button>
+                </div>
+                <div class="popup-card">
+                    <div class="promocion-imagen"><img src="images/hawaiana .jpeg" class="promocion-imagen" /></div>
+                    <h3>Pizza Hawaiana</h3>
+                    <button>Agregar al carrito</button>
+                </div>
+            </div>
+            <button class="popup-back-button" onclick="Swal.close()">Regresar</button>
+        `,
+        showConfirmButton: false,
+        width: '800px', // Ancho del popup
+        padding: '20px',
+        background: '#FF5A5F',
+    });
+}
